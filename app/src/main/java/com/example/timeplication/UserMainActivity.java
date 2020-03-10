e12ec76c901471d16027166b1fe495737508e317
Gastón Rossi <gaston.rossi@basetis.com>
2020-03-10 19:35:12 +0100

    Drawer created

---
+++ app/src/main/java/com/example/timeplication/UserMainActivity.java
@@ -0,0 +1,31 @@
+package com.example.timeplication;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class UserMainActivity extends AppCompatActivity {
+    private AppBarConfiguration mAppBarConfiguration;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_view)
+                .setDrawerLayout(drawer).build();
+
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+    }
+}
