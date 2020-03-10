1c73986af787d9839614649809f4d68f575f138a
Gastón Rossi <gaston.rossi@basetis.com>
2020-03-10 17:30:06 +0100

    Login prepared for next Fragment

--- app/src/main/java/com/example/timeplication/login/LoginFragment.java
+++ app/src/main/java/com/example/timeplication/login/LoginFragment.java
@@ -4,11 +4,14 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.timeplication.R;
 import com.example.timeplication.databinding.LoginFragmentBinding;
@@ -16,6 +19,10 @@ import com.example.timeplication.databinding.LoginFragmentBinding;
 public class LoginFragment extends Fragment {
 
     private LoginViewmodel viewModel;
+    private EditText userNameText;
+    private EditText passwordText;
+    private Button loginButton;
+    private FragmentTransaction transaction;
 
     @Nullable
     @Override
@@ -30,4 +37,25 @@ public class LoginFragment extends Fragment {
         return binding.getRoot();
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        userNameText = view.findViewById(R.id.user_edit_text);
+        passwordText = view.findViewById(R.id.password_edit_text);
+        loginButton = view.findViewById(R.id.login_button);
+        setListener();
+    }
+
+    private void setListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Fragment fm = new LoginFragment();
+                transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, fm);
+                transaction.addToBackStack(null).commit();
+            }
+        });
+    }
 }
